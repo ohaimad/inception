d88d2e4168c3cb517dbbcd7d29597769b84ad2bd
Otmane Haimad <ohaimad@e2r6p6.1337.ma>
2024-10-21 22:07:30 +0100

    lkhawar

--- srcs/mariadb/script.sh
+++ srcs/mariadb/script.sh
@@ -7,16 +7,16 @@ sed -i -e "s/127.0.0.1/0.0.0.0/g" "/etc/mysql/mariadb.conf.d/50-server.cnf"
 service mariadb start
 
 # Create the database if it does not exist
-mariadb -uroot -p"root" -e "CREATE DATABASE IF NOT EXISTS \`ll\`;"
+mariadb -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
 
 # Create the user if it does not exist
-mariadb -uroot -p"root" -e "CREATE USER IF NOT EXISTS 'ohaimad'@'%' IDENTIFIED BY '0000';"
+mariadb -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
 # Grant all privileges to the user on the specified database
-mariadb -uroot -p"root" -e "GRANT ALL PRIVILEGES ON \`ll\`.* TO 'ohaimad'@'%' WITH GRANT OPTION;"
+mariadb -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
 
 # Reload privileges to apply the changes
-mariadb -uroot -p"root" -e "FLUSH PRIVILEGES;"
+mariadb -uroot -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 # Stop MariaDB service
 service mariadb stop
--- srcs/wordpress/tools/script.sh
+++ srcs/wordpress/tools/script.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-cli/builds/gh-pages/phar/wp-cli.phar 
+cd $WP_PATH || exit
 
-chmod +x wp-cli.phar 
-
-mv wp-cli.phar /usr/local/bin/wp
-
-
+# Download WordPress core
 wp core download --allow-root
 
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# Wait for a moment to ensure the download completes
+sleep 10
 
-mv /wp-config.php /var/www/html/wp-config.php
+# Create wp-config.php file
+wp config create --allow-root \
+    --dbname="ll" \
+    --dbuser="ohaimad" \
+    --dbpass="0000" \
+    --dbhost="$WORDPRESS_DB_HOST:3306"
 
-sed -i -r "s/db1/ll/1"   wp-config.php
-sed -i -r "s/user/ohaimad/1"  wp-config.php
-sed -i -r "s/pwd/0000/1"    wp-config.php
+# Install WordPress
+wp core install --allow-root \
+    --url="localhost" \
+    --title="TITLE_WORDPRESS" \
+    --admin_user="ADMIN_WORDPRESS" \
+    --admin_password="ADMIN_PASSWORD_WORDPRESS" \
+    --admin_email="ADMIN_EMAIL_WORDPRESS"
 
-wp core install --url="https://localhost:443"/ --title="WP_TITLE" --admin_user="WP_ADMIN_USR" --admin_password="WP_ADMIN_PWD" --admin_email="WP_ADMIN_EMAIL" --skip-email --allow-root
+# Create a WordPress user
+wp user create --allow-root \
+    "WORDPRESS_USER" "WORDPRESS_USER_EMAIL" \
+    --user_pass="WORDPRESS_USER_PASSWORD"
 
-wp user create "WP_USR" "WP_EMAIL" --role=author --user_pass="WP_PWD" --allow-root
- 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-mkdir /run/php
-
-/usr/sbin/php-fpm7.3 -F
+# Start PHP-FPM
+exec php-fpm7.4 -F
